1c34c6b621abfbb145f5277edf4f84a333eb57de
Florida915 <123209134+Florida915@users.noreply.github.com>
2025-02-24 07:30:52 -0700

    Update casa_8.py

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,16 @@
+import pandas as pd
 
+df = pd.read_csv("big-mac-full-index.csv")
+
+print("Using iterrows() method:")
+for index, row in df.iterrows():
+    big_mac_index = row['local_price'] / row['dollar_ex']
+    print(row['name'], "- Big Mac Index:", big_mac_index)
+
+def calculate_big_mac_index(row):
+    return row['local_price'] / row['dollar_ex']
+
+print("\nUsing apply() method:")
+df['Big Mac Index'] = df.apply(calculate_big_mac_index, axis=1)
+for index, row in df.iterrows():
+    print(row['name'], "- Big Mac Index:", row['Big Mac Index'])
